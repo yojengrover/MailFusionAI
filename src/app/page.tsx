f672997d02312ca1872f844f986d8a21c013541a
yojengrover <yojithegreat@gmail.com>
2025-01-08 11:28:47 +0530

    clerk Auth integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 
 
+import { Button } from "@/components/ui/button";
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  return<div><h1 className="text-red-500">Hello </h1>
+  return<div>
+  <Button>Hello</Button>
   </div>
 }
