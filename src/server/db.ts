39777785a368d5bf57c47469697b9bd30471b450
yojengrover <yojithegreat@gmail.com>
2025-01-25 14:21:37 -0600

    database created

--- src/server/db.ts
+++ src/server/db.ts
@@ -5,7 +5,7 @@ import { env } from "@/env";
 const createPrismaClient = () =>
   new PrismaClient({
     log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      env.NODE_ENV === "development" ? [ "error", "warn"] : ["error"],
   });
 
 const globalForPrisma = globalThis as unknown as {
@@ -15,3 +15,4 @@ const globalForPrisma = globalThis as unknown as {
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
